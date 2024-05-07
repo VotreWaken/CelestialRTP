c02c9833c8c50c085fff4747e530e4f7944f2c0a
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-05-07 23:41:47 +0300

    Initialization of basic settings for Multiple startup projects, and added initial unit testing

--- CelestialRTP/obj/Debug/net8.0/CelestialRTP.AssemblyInfo.cs
+++ CelestialRTP/obj/Debug/net8.0/CelestialRTP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CelestialRTP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f3d16c9fe2c14675dbc3d2c20ed04315c581a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CelestialRTP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CelestialRTP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
